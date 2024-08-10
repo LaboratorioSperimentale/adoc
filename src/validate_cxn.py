9cf93a5d18b9c8c2a0497df54f9c176b1a755336
Ludovica Pannitto <ellepannitto@gmail.com>
2024-08-10 16:40:46 +0200

    fixed cxns and added source to examples

--- src/validate_cxn.py
+++ src/validate_cxn.py
@@ -3,30 +3,31 @@ from pathlib import Path
 
 import cerberus
 import yaml
-from sty import fg
+from sty import fg, bg
 from pybtex.database import parse_file
+
+import utils as u
 
 CC_DB = yaml.safe_load(open("cc-database/cc-database.yaml", encoding="utf-8"))
 CC_LIST = {x["Id"]:x["Type"] for x in CC_DB}
 BIB = parse_file("bibliography/entries.bib", bib_format="bibtex")
 
-
 class CXNValidator(cerberus.Validator):
 
 	def _check_with_CClist_construction(self, field, value):
-
 		if value is not None:
 			if value.startswith("cc"):
 				cc, category, name_str = value.split(":")
 				cc_id = f"{category}:{name_str}"
 				if not category in ["cxn", "str"]:
-					self._error(field, f"Value '{cc_id}' has type '{CC_LIST[cc_id]}' in list of comparative concepts, types 'str' or 'cxn' required")
+					self._error(field, f"(W) Value '{cc_id}' has type '{CC_LIST[cc_id]}' in list of comparative concepts, types 'str' or 'cxn' required")
 
 				if not cc_id in CC_LIST:
 					self._error(field, f"Value '{cc_id}' not in list of comparative concepts")
 
 			else:
-				self._error(field, f"Value '{value}' created by author")
+				self._error(field, f"(W) Value '{value}' defined by author")
+				# self._warnings.append((field, f"DOUBLE CHECK: Value '{value}' created by author"))
 
 
 	def _check_with_CClist_meaning(self, field, value):
@@ -36,13 +37,13 @@ class CXNValidator(cerberus.Validator):
 				cc, category, name_str = value.split(":")
 				cc_id = f"{category}:{name_str}"
 				if not category in ["inf", "sem"]:
-					self._error(field, f"Value '{cc_id}' has type '{CC_LIST[cc_id]}' in list of comparative concepts, types 'str' or 'cxn' required")
+					self._error(field, f"(W) Value '{cc_id}' has type '{CC_LIST[cc_id]}' in list of comparative concepts, types 'inf' or 'sem' required")
 
 				if not cc_id in CC_LIST:
 					self._error(field, f"Value '{cc_id}' not in list of comparative concepts")
 
 			else:
-				self._error(field, f"Value '{value}' created by author")
+				self._error(field, f"(W) Value '{value}' defined by author")
 
 
 
@@ -63,8 +64,6 @@ class CXNValidator(cerberus.Validator):
 		if not value in BIB.entries:
 			self._error(field, f"Value '{value}' is not a valid bibliographical identifier'")
 
-
-
 if __name__ == "__main__":
 
 	new_files = sys.argv[1:]
@@ -76,42 +75,69 @@ if __name__ == "__main__":
 		with open(file, encoding="utf-8") as stream:
 			try:
 				cxn = yaml.safe_load(stream)
-
-				validation_test = v.validate(cxn)
-
-				if validation_test:
-					output_str = fg.green + \
-						f"[PASSED] CONSTRUCTION N. {Path(file).stem.split('_')[1]}: {cxn['name']}" + \
-							fg.rs
-				else:
-					output_str = fg(255, 10, 10) + \
-						f"[FAILED] CONSTRUCTION N. {Path(file).stem.split('_')[1]}: {cxn['name']}" + \
-							fg.rs
-
-				print(output_str)
-
-
-				for field, value in v.errors.items():
-
-					print(f"WARNING: {field}")
-					for x in value:
-						if len(x) > 0:
-							if type(x) == str:
-								print(f"\t{x}")
-							else:
-								for element in x:
-									if type(element) == int:
-										print(f"\titem-{element}: {' - '.join(x[element])}")
-						else:
-							print(f"\t{x}")
-						n_warnings += 1
-					print()
-				print()
-
 			except yaml.YAMLError as exc:
 				print(exc)
 
-	if n_warnings > 0:
-		print(f"During check {n_warnings} warnings have been detected. Please check your files!")
-	print()
+
+			print(f"Examining CXN ID. {Path(file).stem.split('_')[1]}: {cxn['name']}")
+
+			validation_test = v.validate(cxn)
+
+			if validation_test:
+				output_str = fg.green + \
+					f"[PASSED] CONSTRUCTION N. {Path(file).stem.split('_')[1]}: {cxn['name']}" + \
+						fg.rs
+
+			else:
+
+				n_errors = 0
+				n_warning = 0
+
+				for field, value in v.errors.items():
+					print(f"Issue with field {field}")
+					for x in value:
+						if len(x) > 0:
+							if type(x) == str:
+								if x.startswith("(W)"):
+									n_warnings += 1
+								else:
+									n_errors += 1
+								print(f"\t{x}")
+							else:
+								for element, err_string in x.items():
+									err_string = err_string[0]
+									if err_string.startswith("(W)"):
+										n_warnings += 1
+									else:
+										n_errors += 1
+									print(f"\t{err_string}")
+						else:
+							if x.startswith("(W)"):
+								n_warnings += 1
+							else:
+								n_errors += 1
+							print(f"\t{x}")
+					# print()
+				print()
+
+				if n_errors > 0:
+					output_str = fg(255, 10, 10) + \
+						f"[FAILED] CONSTRUCTION N. {Path(file).stem.split('_')[1]}: {cxn['name']}" + \
+							fg.rs
+				else:
+					output_str = fg.blue + \
+						f"[WARNING] CONSTRUCTION N. {Path(file).stem.split('_')[1]}: {cxn['name']}" + \
+							fg.rs
+
+			print(output_str)
+			print()
+
+
+
+
+
+
+	# if n_warnings > 0:
+	# 	print(f"During check {n_warnings} warnings have been detected. Please check your files!")
+	# print()
 
