14fe1edeb4ab32943dadc699127ef71325e3c55e
ellepannitto <ellepannitto@gmail.com>
2025-09-03 11:09:43 +0200

    add basic parser for conllc

---
+++ src/tools.py
@@ -0,0 +1,29 @@
+
+def parse_token(cols):
+    header = ["id", "form", "lemma", "upos", "feats", "head", "deprel", "required", "without", "sem_feats", "sem_roles", "adjacency", "identity"]
+    return {x:y for x, y in zip(header, cols)}
+
+
+
+# TODO: handle more than one construction
+def parse(construction):
+
+	ret = { "metadata": [],
+			"tokens": []
+		}
+
+	for line in construction:
+		line = line.strip()
+		if len(line):
+			if line.startswith("#"):
+				ret["metadata"].append(line[1:].split("="))
+			else:
+				new_token = parse_token(line.split("\t"))
+				ret["tokens"].append(new_token)
+
+	return ret
+
+
+if __name__ == "__main__":
+    with open("/Users/ludovica/Documents/projects/adoc/cxns_conllc/cxn_68.conllc") as fin:
+        print(parse(fin.readlines()))
